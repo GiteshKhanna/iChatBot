43ffc8478d1ee6138734e571ae05a3ee7746e3c6
unknown <gitesh.khanna97@gmail.com>
2018-06-23 06:19:15 +0530

    question dictionary for classification-Alpha

---
+++ ques_dictionary.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+ques_dict = {0: 'What makes you angry?',
+             1: 'Can you work under pressure?',
+             2: 'Why should I hire you?',
+             3: 'Tell me more about your goals.',
+             4: 'What was the toughest decision you ever had to make?'
+            }
+

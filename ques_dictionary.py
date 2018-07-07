6a5589fe25ad25dcc9e004dd2e57662efaaf2cdf
unknown <gitesh.khanna97@gmail.com>
2018-07-07 07:33:26 +0530

    Added data for better clarity

--- ques_dictionary.py
+++ ques_dictionary.py
@@ -4,6 +4,7 @@ ques_dict = {0: 'What makes you angry?',
              1: 'Can you work under pressure?',
              2: 'Why should I hire you?',
              3: 'Tell me more about your goals.',
-             4: 'What was the toughest decision you ever had to make?'
+             4: 'What was the toughest decision you ever had to make?',
+             5: 'AI and machine learning is hyped all around the world. What makes you stand out from the other candidates?'
             }
 
